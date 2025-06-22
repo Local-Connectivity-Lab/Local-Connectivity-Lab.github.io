a7f8ebfd1ca57f53e8beac4755ca2e90a4f49578
Kevin Cameron <kevinacameron@hotmail.com>
2025-06-22 09:14:50 -0700

    gh pages prep

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({
-		fallback: '200.html'
-	}) }
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
 export default config;
