d2255acfdda4a068d0ea94f421c3581bb0daea1d
Kevin Cameron <kevinacameron@hotmail.com>
2025-06-28 19:49:09 +0100

    buh bye tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { paraglideVitePlugin } from '@inlang/paraglide-js';
-import tailwindcss from '@tailwindcss/vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
-		tailwindcss(),
 		sveltekit(),
 		devtoolsJson(),
 		paraglideVitePlugin({
