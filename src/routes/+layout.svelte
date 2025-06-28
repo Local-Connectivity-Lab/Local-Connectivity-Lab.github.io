074ceff237a81279f23378e68215003f1db17ab6
Kevin Cameron <kevinacameron@hotmail.com>
2025-06-28 19:49:15 +0100

    nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import { m } from '$lib/paraglide/messages.js';
 	import { setLocale } from '$lib/paraglide/runtime';
+
+	let currentUrl = $state('');
+
+	page.subscribe(($page) => {
+		currentUrl = $page.url.pathname;
+	});
 
 	import '../app.css';
 
@@ -9,44 +16,28 @@
 	const navLength = 8;
 </script>
 
-<header
-	class="
-		p-[0.5em]
-		px-[2em]
-	"
->
-	<a
-		href="/"
-		 class="
-			flex
-			items-center
-			start
-			gap-2
-
-			text-white
-			text-[2em]
-			font-medium
-		"
-	>
-		<img
-			src="/logo.min.png"
-			alt=""
-			class="
-				h-[2em]
-				aspect-square
-			"
-		>
+<header>
+	<a href="/" class="logo-link">
+		<img src="/logo.min.png" alt="">
 		<h1>{m['common.scn']()}</h1>
 	</a>
 </header>
 
 <nav>
 	{#each Array(navLength).fill(0) as _, i}
-		<a href={m[`common.nav.${i}.href`]()}>
-			{m[`common.nav.${i}.label`]()}
+		<a
+			href={m[`common.nav.${i}.href`]()}
+			class="nav-link"
+			class:active={m[`common.nav.${i}.href`]() === currentUrl}
+		>
+			<span>
+				{m[`common.nav.${i}.label`]()}
+			</span>
 		</a>
 	{/each}
 </nav>
+
+{currentUrl}
 
 {@render children()}
 
@@ -83,7 +74,74 @@
 
 <style lang="postcss">
 	header {
+		padding: 0.5em 2em;
 		background-color: var(--color-brand-primary);
+	}
+
+	.logo-link {
+		display: flex;
+		align-items: center;
+		gap: 0.5em;
+
+		text-decoration: none;
+
+		h1 {
+			margin: 0;
+
+			color: white;
+			font-size: 2.2em;
+			font-weight: 500;
+		}
+		img {
+			height: 4em;
+			aspect-ratio: 1 / 1;
+			object-fit: contain;
+		}
+	}
+
+
+	nav {
+		--nav-border: solid 1px var(--color-nav-border);
+
+		display: block;
+		padding: 0.5em 2em;
+
+		border-left: var(--nav-border);
+		border-right: var(--nav-border);
+
+		a {
+			display: inline-block;
+
+			color: var(--color-nav-text);
+			text-decoration: none;
+
+			border-right: var(--nav-border);
+
+			&:first-child {
+				border-left: var(--nav-border);
+			}
+
+			&:hover span {
+				padding: 0.5em 2em;
+				margin: 0;
+
+				background-color: var(--color-nav-background);
+			}
+
+			&.active span {
+				background-color: var(--color-nav-background);
+			}
+
+
+		}
+
+		span {
+			display: inline-block;
+			padding: 0.5em 1em;
+			margin: 0 1em;
+
+			transition: margin 200ms, padding 200ms;
+		}
 	}
 
 </style>
