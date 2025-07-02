9e9c78d9407be94572441dd1eb935df7bab0d2f3
Kevin Cameron <kevinacameron@hotmail.com>
2025-07-02 23:55:10 +0100

    consistent spaces

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,11 +2,11 @@ import type { Handle } from '@sveltejs/kit';
 import { paraglideMiddleware } from '$lib/paraglide/server';
 
 const handleParaglide: Handle = ({ event, resolve }) => paraglideMiddleware(event.request, ({ request, locale }) => {
-	event.request = request;
+    event.request = request;
 
-	return resolve(event, {
-		transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale)
-	});
+    return resolve(event, {
+        transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale)
+    });
 });
 
 export const handle: Handle = handleParaglide;
