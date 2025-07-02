947886a7ab6451e0f2242acdbb4fce4a4628a770
Kevin Cameron <kevinacameron@hotmail.com>
2025-07-02 17:31:10 +0100

    separate nav component

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	import { m }         from '$lib/paraglide/messages.js';
+	import { setLocale } from '$lib/paraglide/runtime';
+
+	let currentUrl = $state('');
+
+	page.subscribe(($page) => {
+		currentUrl = $page.url.pathname;
+	});
+
+	const navLength = 8;
+</script>
+
+<nav>
+	{#each Array(navLength).fill(0) as _, i}
+		<a
+			href={m[`common.nav.${i}.href`]()}
+			class="nav-link"
+			class:active={m[`common.nav.${i}.href`]() === currentUrl}
+		>
+			<span>
+				{m[`common.nav.${i}.label`]()}
+			</span>
+		</a>
+	{/each}
+</nav>
+
+<style>
+	nav {
+		--nav-border: solid 1px var(--color-nav-border);
+
+		display: block;
+		padding: 0.5em 2em;
+
+		a {
+			display: inline-block;
+
+			color: var(--color-nav-text);
+			text-decoration: none;
+
+			border-right: var(--nav-border);
+
+			&:first-child {
+				border-left: var(--nav-border);
+			}
+
+			&:hover span {
+				padding: 0.5em 2em;
+				margin: 0;
+				text-shadow: 0 0 1px var(--color-nav-text);
+
+				background-color: var(--color-nav-background);
+			}
+
+			&.active span {
+				background-color: var(--color-nav-background);
+			}
+		}
+
+		span {
+			display: inline-block;
+			padding: 0.5em 1em;
+			margin: 0 1em;
+
+			transition: margin 200ms, padding 200ms;
+		}
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,8 @@
 	import { m }         from '$lib/paraglide/messages.js';
 	import { setLocale } from '$lib/paraglide/runtime';
 
+    import Nav from '$lib/components/Nav.svelte';
+
 	let currentUrl = $state('');
 
 	page.subscribe(($page) => {
@@ -24,25 +26,12 @@
 	</a>
 </header>
 
-<nav>
-	{#each Array(navLength).fill(0) as _, i}
-		<a
-			href={m[`common.nav.${i}.href`]()}
-			class="nav-link"
-			class:active={m[`common.nav.${i}.href`]() === currentUrl}
-		>
-			<span>
-				{m[`common.nav.${i}.label`]()}
-			</span>
-		</a>
-	{/each}
-</nav>
+<Nav />
 
-{currentUrl}
 
 {@render children()}
 
-<div>
+<footer>
 	<div>
 		<h5>{m['common.address.header']()}</h5>
 		<p>{m['common.address.physical']()}</p>
@@ -71,7 +60,7 @@
 	</div>
 
 	<button onclick={() => setLocale('en')}>en</button>
-</div>
+</footer>
 
 <style lang="postcss">
 	header {
@@ -97,49 +86,6 @@
 			height: 4em;
 			aspect-ratio: 1 / 1;
 			object-fit: contain;
-		}
-	}
-
-
-	nav {
-		--nav-border: solid 1px var(--color-nav-border);
-
-		display: block;
-		padding: 0.5em 2em;
-
-		a {
-			display: inline-block;
-
-			color: var(--color-nav-text);
-			text-decoration: none;
-
-			border-right: var(--nav-border);
-
-			&:first-child {
-				border-left: var(--nav-border);
-			}
-
-			&:hover span {
-				padding: 0.5em 2em;
-				margin: 0;
-				text-shadow: 0 0 1px var(--color-nav-text);
-
-				background-color: var(--color-nav-background);
-			}
-
-			&.active span {
-				background-color: var(--color-nav-background);
-			}
-
-
-		}
-
-		span {
-			display: inline-block;
-			padding: 0.5em 1em;
-			margin: 0 1em;
-
-			transition: margin 200ms, padding 200ms;
 		}
 	}
 
