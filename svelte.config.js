cc483ae20b2ee79fb4fda5c1bba8b4743c23a557
Kevin Cameron <kevinacameron@hotmail.com>
2025-07-04 10:58:14 -0700

    cleanup

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
             fallback: '404.html'
         }),
         paths: {
-            base: process.argv.includes('dev') ? '/Local-Connectivity-Lab.github.io' : process.env.BASE_PATH
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
         }
     }
 };
