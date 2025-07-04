51854c6cedee4ee801343d491347f3141ce56654
Kevin Cameron <kevinacameron@hotmail.com>
2025-07-04 11:07:48 -0700

    fix build base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: process.env.BASE_PATH ?? '/',
     plugins: [
         sveltekit(),
         devtoolsJson(),
