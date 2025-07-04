060e829cc0abc4a69cf52c8ce66a6c72b21f63e7
Kevin Cameron <kevinacameron@hotmail.com>
2025-07-04 13:52:41 -0700

    remove vite base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: process.env.BASE_PATH ?? '/',
     plugins: [
         sveltekit(),
         devtoolsJson(),
