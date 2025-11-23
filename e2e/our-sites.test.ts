6a8aab696c08b7bfdb67cd5ae118df0320f3937a
Kevin Cameron <kevinacameron@hotmail.com>
2025-11-23 15:04:10 -0800

    Our Sites tests

--- e2e/our-sites.test.ts
+++ e2e/our-sites.test.ts
@@ -1,15 +1,46 @@
 import { expect, test } from '@playwright/test';
+import { expectH1AndTitle, expectSections, expectLinks } from './lib/lib';
 
 test.beforeEach(async ({ page }) => {
     await page.goto('/our-sites');
 });
 
-test.describe('Our Sites Page', () => {
-    test('our sites page has expected content', async ({ page }) => {
-        await expect(page.locator('data-test=our-sites-page')).toBeVisible();
-        await expect(page.locator('main h1')).toBeVisible();
-        await expect(page).toHaveTitle(/get connected/i);
+test.describe('Our Sites', () => {
+    test('has expected content', async ({ page }) => {
+        await expectSections({
+            page,
+            pageName: 'our-sites',
+            sections: [
+                'overview',
+                'details',
+                'eligibility',
+                'locations',
+                'map',
+            ]
+        });
+
+        await expectH1AndTitle({
+            page,
+            h1 : "Get Connected",
+            title: "Get Connected - Seattle Community Network"
+        });
     });
 
+    // skip for now, destination page is down
+    test.skip('main CTAs link correctly', async ({ page }) => {
+        await expectLinks({
+            page,
+            pageName: 'our-sites',
+            links: [
+                {
+                    linkLocator: 'map-cta',
+                    destLocator: 'h1'
+                }
+            ]
+        });
+    });
 
+    test('google map iframe is loaded', async ({ page }) => {
+        await expect(page.locator('iframe[src*="google.com/maps"]')).toBeVisible();
+    });
 });
