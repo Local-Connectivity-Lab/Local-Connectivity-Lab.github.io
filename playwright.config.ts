cfb170592bd8b7ef60534d9af5d4cac05f4db07b
Kevin Cameron <kevinacameron@hotmail.com>
2025-11-22 20:39:43 -0800

    dont stand up server when running the tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-    webServer: {
-        command: 'npm run build && npm run preview',
-        port: 4173
+    // webServer: {
+    //     command: 'npm run build && npm run preview',
+    //     port: 4173
+    // },
+    use: {
+        baseURL: "http://localhost:5173"
     },
     testDir: 'e2e'
 });
