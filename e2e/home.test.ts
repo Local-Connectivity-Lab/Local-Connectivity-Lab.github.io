47f8d3afebe7fc12fad4c5e85e53da8dc8d5d10a
Kevin Cameron <kevinacameron@hotmail.com>
2025-11-23 14:50:12 -0800

    updated home tests

--- e2e/home.test.ts
+++ e2e/home.test.ts
@@ -1,43 +1,57 @@
 import { expect, test } from '@playwright/test';
+import { expectH1AndTitle, expectLinks, expectSections } from './lib/lib';
 
 test.beforeEach(async ({ page }) => {
     await page.goto('/');
 });
 
 test.describe('Home Page', () => {
-    test('home page has expected content', async ({ page }) => {
-        const locators = [
-            'data-test=home-carousel',
-            'data-test=home-hero',
-            'data-test=home-building-image',
-            'data-test=home-overview',
-            'data-test=home-get-involved',
-            'data-test=home-calendar',
-            'data-test=home-partners',
-        ];
+    test('has expected content', async ({ page }) => {
+        await expectSections({
+            page,
+            pageName: 'home',
+            sections: [
+                'carousel',
+                'hero',
+                'building-image',
+                'overview',
+                'get-involved',
+                'calendar',
+                'partners',
+            ]
+        });
 
-        await expect(page.locator('header h1')).toBeVisible();
-        await expect(page).toHaveTitle(/seattle community network/i);
-
-        for (const locator of locators) {
-            await expect(page.locator(locator)).toBeVisible();
-        }
+        await expectH1AndTitle({
+            page,
+            h1 : "Seattle Community Network",
+            title: "Seattle Community Network"
+        });
     });
 
-    test('get involved button links to docs', async ({ page }) => {
-        const getInvolvedButton = page.locator('data-test=home-hero-cta-button');
-
-        await getInvolvedButton.click();
-
-        await expect(page.locator('h1#join-us')).toBeVisible();
-    });
-
-    test('about us button links to about page', async ({ page }) => {
-        const aboutUsButton = page.locator('data-test=home-overview-about-button');
-
-        await aboutUsButton.click();
-
-        await expect(page.locator('h1#seattle-community-network-docs')).toBeVisible();
+    test('main CTAs link correctly', async ({ page }) => {
+        await expectLinks({
+            page,
+            pageName: 'home',
+            links: [
+                {
+                    linkLocator: 'hero-cta',
+                    destLocator: 'h1#join-us'
+                },
+                {
+                    linkLocator: 'overview-cta',
+                    destLocator: 'h1#seattle-community-network-docs'
+                },
+                {
+                    linkLocator: 'involved-volunteer-cta',
+                    destLocator: 'h1#join-us'
+                },
+                {
+                    linkLocator: 'involved-connect-cta',
+                    destLocator: 'h1',
+                    destText : /Our Sites|Get Connected/
+                }
+            ]
+        });
     });
 
     test('google calendar iframe is loaded', async ({ page }) => {
