220e075d31a6e30d05da1ab369d6b1c6ea46999b
Kevin <kevinacameron@hotmail.com>
2026-01-16 22:05:04 -0800

    meta/og tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
     import '../app.css';
 
-    import Header from '$lib/components/Header.svelte';
-    import Footer from '$lib/components/Footer.svelte';
+    import Header   from '$lib/components/Header.svelte';
+    import Footer   from '$lib/components/Footer.svelte';
+    import Metatags from '$lib/components/Metatags.svelte';
 
     let { children } = $props();
 </script>
+
+<Metatags />
 
 <Header />
 
