fed1163620dae589ac27490cd5cd4bb61758acb7
Kevin Cameron <kevinacameron@hotmail.com>
2026-01-29 07:59:17 -0800

    meta/og tags (#35)
    merged 1/29/26 by ted

---
+++ src/lib/components/Metatags.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { page } from '$app/state';
+
+    import { m } from '$lib/paraglide/messages.js';
+
+    import image from '$lib/assets/og-image.jpg';
+
+    const og = {
+        title : m['meta.title'](),
+        description : m['meta.description'](),
+        url : m['meta.url']() + page.url.pathname,
+        type : m['meta.type'](),
+        image
+    };
+</script>
+
+<svelte:head>
+
+    <meta name="description" content="{m['meta.description']()}" />
+
+    {#each Object.entries(og) as [key, value]}
+        <meta property={`og:${key}`} content={value} />
+    {/each}
+
+</svelte:head>
