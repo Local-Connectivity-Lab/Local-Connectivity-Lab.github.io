2ac32eb7009fd6d68d3d42bbc8cef13167333e6f
Kevin Cameron <kevinacameron@hotmail.com>
2026-03-01 13:34:15 -0800

    better redirects and handle /index.html as well (#50)

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,11 @@
+import { browser } from '$app/environment';
+import { goto } from '$app/navigation';
+
+if (browser) {
+    // Handle index.html redirect on client side
+    if (window.location.pathname === '/index.html') {
+        goto('/', { replaceState: true });
+    }
+}
+
 export const prerender = true;
